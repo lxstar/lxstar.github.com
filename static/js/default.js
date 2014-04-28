2f7273d9a289c9e3c70795f2c4c486255d6201f8
lxstar <lxstar01@gmail.com>
2014-04-28 18:41:26 +0800

    first test

---
+++ static/js/default.js
@@ -0,0 +1,11 @@
+// $(function(){
+
+// 	$('.nav_node').mouseenter(function(){
+// 		$(this)[0].className = 'nav_node_active';
+// 		//$(this).transition('bounce');
+// 	});
+// 	$('.nav_node').mouseout(function(){
+// 		$(this)[0].className = 'nav_node';
+// 		$(this).transition('bounce');
+// 	});
+// });
